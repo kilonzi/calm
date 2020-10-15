135fb3cd21ddfd362cfc47ae5f97850f016cfa1d
kilonzi <mrjohnkilonzi@gmail.com>
2020-10-15 13:05:08 +0300

    Added ability to get the x axis max number

--- src/robot.py
+++ src/robot.py
@@ -12,7 +12,28 @@ class Robot:
     def get_condition(self):
         return self.condition
 
-    def 
+    def get_bound(self):
+        x_bound = 0
+        sum_bound_ints = 0
+        if self.condition == 0:
+            return 0
+        elif self.condition == 1:
+            return 1
+        else:
+            while sum_bound_ints<self.condition+1:
+                 x_bound += 1
+                 sum_bound_ints = self.sum_of_digits(x_bound)
+        return x_bound-1
+
+    def sum_of_digits(self,n):
+        lst = self.split_digits(n)
+        return sum(lst)
+
+    def split_digits(self,n):
+        return [int(d) for d in str(n)]
+
+     
+
 
 
 robot = Robot(10)
--- tests/test_robot.py
+++ tests/test_robot.py
@@ -21,3 +21,25 @@ def test_robot_returns_correct_condition():
     expected = 23
     actual = robot.get_condition()
     assert expected == actual, "The robot returns correct condition"
+
+def test_x_bound_returns_int():
+    robot = Robot(20)
+    actual = robot.get_bound()
+    assert isinstance(actual,int), "Bound should be an int"
+
+def test_x_bound_returns_zero():
+    robot = Robot(0)
+    actual = robot.get_bound()
+    assert actual == 0 , "Bound should be a Zero"
+
+def test_x_bound_return_one():
+    robot = Robot(1)
+    actual = robot.get_bound()
+    assert actual == 1 , "Bound should be a One"
+
+def test_x_bound_return_correctly():
+    robot = Robot(10)
+    expected = 28
+    actual = robot.get_bound()
+    assert actual == expected , "Bound should be a Twenty Eight"
+
